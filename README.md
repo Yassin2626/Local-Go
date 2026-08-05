fc099f29b990211664a07a649d88b2ef5165e3de
Yassin2626 <sinogames2626@gmail.com>
2026-08-04 22:05:04 -0400

    remove old project files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Local-Go
-This is the GitHub repo for the group 5, for the app ("Local Go")
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-library;
-
-import 'package:flutter/material.dart';
-import 'app.dart';
-
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const App());
-}
